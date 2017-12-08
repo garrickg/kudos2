6565875a444261edeee39cdfa78dce78f6e5ae7d
Garrick Gunn <ggunn@starfishmedical.com>
2017-12-08 13:27:43 -0800

    fix nominee name indexing

--- src/components/kudos.js
+++ src/components/kudos.js
@@ -89,7 +89,7 @@ class Kudos extends Component {
     // Copy current list of nominees
     const nominees = [...this.state.nominees]
     const nomineeKey = e.target.dataset.key
-    const nomineeIndex = this.state.people.find(person => person.key == nomineeKey)
+    const nomineeIndex = this.state.people.findIndex(person => person.key == nomineeKey)
     const nominee = {
       First: this.state.people[nomineeIndex].First,
       Last: this.state.people[nomineeIndex].Last,
